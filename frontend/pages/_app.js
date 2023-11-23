3410fc9364f3a595688e2628ac9f1f2e6023ce8f
samtu <41340170+SattuSupari21@users.noreply.github.com>
2023-11-23 17:22:35 +0530

    project pushed from local storage to GitHub

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,5 +1,29 @@
-import '@/styles/globals.css'
+import "../styles/globals.css";
+import { createClient, cacheExchange, fetchExchange  } from "@urql/core";
+import { Provider } from "urql";
+import Nav from "../components/Nav";
+import { StateContext } from "../lib/context";
+import { UserProvider } from "@auth0/nextjs-auth0/client";
+import { Toaster } from "react-hot-toast";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+// const client = createClient({ url: process.env.NEXT_PUBLIC_BACKEND_API });
+
+const client = createClient({
+    url: "http://localhost:1337/graphql",
+    exchanges: [cacheExchange, fetchExchange],
+});
+function MyApp({ Component, pageProps }) {
+  return (
+      <UserProvider>
+        <StateContext>
+          <Provider value={client}>
+            <Toaster />
+            <Nav />
+            <Component {...pageProps} />
+          </Provider>
+        </StateContext>
+      </UserProvider>
+  );
 }
+
+export default MyApp;
